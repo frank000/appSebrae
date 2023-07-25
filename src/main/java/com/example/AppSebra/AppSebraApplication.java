83507ed1081e3bcff537842c9cbbbbc8a772d421
Franklim Paulino <franklim.ti@gmail.com>
2023-07-25 14:22:11 -0300

    Initial Commit

---
+++ src/main/java/com/example/AppSebra/AppSebraApplication.java
@@ -0,0 +1,13 @@
+package com.example.AppSebra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSebraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSebraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AppSebra/AppSebraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AppSebra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSebraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
